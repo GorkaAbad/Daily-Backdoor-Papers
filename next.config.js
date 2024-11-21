29efd2bf0475859999db46bcde773b6eba804afd
GorkaAbad <gorkaabad12@gmail.com>
2024-11-21 15:00:07 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     unoptimized: true,
+//   },
+//   basePath: '/backdoor-papers',
+//   assetPrefix: '/backdoor-papers/',
+// }
+
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/backdoor-papers',
-  assetPrefix: '/backdoor-papers/',
-}
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
